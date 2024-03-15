17db12f884422a6996ba01971fc4be061e896d7c
Victor Hanson <vectoralarm@gmail.com>
2024-03-15 18:47:36 +0100

    Update run.sh

--- hassio-access-point/run.sh
+++ hassio-access-point/run.sh
@@ -219,7 +219,7 @@ fi
 logger "## Starting hostapd daemon" 1
 # If debug level is greater than 1, start hostapd in debug mode
 if [ $DEBUG -gt 1 ]; then
-    hostapd -d /hostapd.conf & wait ${!}
+    hostapd -dd /hostapd.conf & wait ${!}
 else
     hostapd /hostapd.conf & wait ${!}
 fi
